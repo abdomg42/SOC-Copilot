e5a75b18ed25c8f7e5101c71bb94da737ff26b82
abdomg42 <sonysam3a@gmail.com>
2026-04-18 01:50:22 +0100

    test driver

--- agent/graph_db.py
+++ agent/graph_db.py
@@ -4,3 +4,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
+
+_driver = None
+def get_driver():
+    global _driver
+    if _driver is None:
+        _driver = GraphDatabase.driver(
+            os.getenv('NEO4J_URI'),
+            auth=(os.getenv('NEO4J_USER'), os.getenv('NEO4J_PASSWORD'))
+        )
+    return _driver
+
